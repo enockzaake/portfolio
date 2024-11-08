e5de84bea60eefbe9f22970d7fc491773c09147c
enockzaake <enockzaake@gmail.com>
2024-11-09 02:30:51 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className=" flex items-center justify-center min-h-screen  text-white">
+      <div className="text-4xl">ZAAKE ENOCK</div>
+    </div>
+  );
+}
